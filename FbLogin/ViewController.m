439ed9ba4f943d1418e7ccdc8be76add3af98f79
Doysoft <lucklui@doysoft.com>
2014-10-23 10:53:11 +0800

    demo

--- FbLogin/ViewController.m
+++ FbLogin/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    // Align the button in the center horizontally
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 5);
+    [self.view addSubview:loginView];
+    
 }
 
 - (void)didReceiveMemoryWarning
